4b8cb84f469205ea90dec2dd1c03668a36720e41
EdiblePerspective <ryancox430@gmail.com>
2024-02-07 11:15:50 +0000

    add primitives

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@
 import * as Switch from '@radix-ui/react-switch';
 import Cursor from './components/Cursor'
 import "./globals.css";
-
+import SliderComp from './components/Slider'
+import ProgressBar from './components/Progress'
 export default function Home() {
   
   return (
@@ -11,6 +12,12 @@ export default function Home() {
 <Switch.Root className='SwitchRoot'>
   <Switch.Thumb className='SwitchThumb' />
 </Switch.Root>
+</div>
+<div>
+  <SliderComp/>
+</div>
+<div>
+    <ProgressBar/>
 </div>
 <Cursor/>
   </>
