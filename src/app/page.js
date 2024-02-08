5acbe7a878463975027940826244870a45132ad8
EdiblePerspective <ryancox430@gmail.com>
2024-02-08 22:56:31 +0000

    hue works

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,28 @@
 "use client"
+//import catImg from '../../public/catImg.jpg'//
 import * as Switch from '@radix-ui/react-switch';
 import * as Progress from '@radix-ui/react-progress';
 import Cursor from './components/Cursor'
 import "./globals.css";
 import SliderComp from './components/Slider'
 import { useState, useEffect} from 'react';
+import Image from 'next/image';
 export default function Home() {
   const [light, setLight] = useState(true)
   const [cat, setCat]=useState(false)
   const [progress, setProgress] = useState(0);
   const [hue, setHue] = useState(0);
+  const catStyle= { 
+    filter: `hue-rotate(${hue}deg)`,
+    objectFit:'cover',
+    position:'absolute',
+    zIndex:'-10' }
   return (
   <>
 
-<body className={cat===true?`cat backdrop-hue-rotate-${hue} hue-rotate-[${hue}deg] `
+<body className={cat===true?'bg-none'//`cat backdrop-hue-rotate-${hue} hue-rotate-[${hue}deg] `//
 :(light===true?'bg-white':'bg-black')} >
+{cat===true?<Image fill={true} src='/catImg.jpg' alt="Cat-Image" style={catStyle}/>:null}
 <div>
 <form>
 <Switch.Root className='SwitchRoot' 
@@ -39,7 +47,9 @@ setLight((light)=>true)
 </Progress.Root>
 </div>
 {cat===false?<Cursor/>:null}
+
+
 </body>
-  </>
+</>
   );
 }
